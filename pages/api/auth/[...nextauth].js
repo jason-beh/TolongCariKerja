1a17533985732cd21bf922ae21865c908c19eaf5
Jason Beh <behjieshen@gmail.com>
2021-08-02 16:05:24 +0800

    Login page with styling

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import User from "../../../models/User";
+import dbConnect from "../../../utils/mongodb";
 
 export default NextAuth({
   providers: [
@@ -14,6 +16,46 @@ export default NextAuth({
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    signIn: async (user, account, profile) => {
+      const { name, email } = user;
+
+      try {
+        await dbConnect();
+        let user = await User.findOne({ email }).exec();
+        if (!user) {
+          const newUser = new User({
+            name,
+            email,
+          });
+          await newUser.save();
+        }
+
+        return Promise.resolve(true);
+      } catch (err) {
+        console.log("Server error");
+        console.log(err);
+        throw new Error("Error in signin callback");
+      }
+    },
+
+    session: async (session, user) => {
+      try {
+        await dbConnect();
+        let dbUser = await User.findOne({ email: session.user.email }).exec();
+        session.dbUser = dbUser;
+        session.id = dbUser._id;
+      } catch (err) {
+        console.log("Server error");
+        console.log(err);
+        throw new Error("Error in session callback");
+      }
+      return Promise.resolve(session);
+    },
+  },
+
+  // Enable debug messages in the console if you are having problems
+  debug: false,
 
   // A database is optional, but required to persist accounts in a database
   //   database: process.env.DATABASE_URL,
--- pages/login.js
+++ pages/login.js
@@ -12,18 +12,49 @@ export default function Login() {
     }
   }, [loading, session]);
 
-  return (
-    <>
-      {!session && (
-        <div className="flex flex-col">
-          <button onClick={() => signIn("google")}>
-            Sign in with Google
-          </button>
-          <button onClick={() => signIn("facebook")}>
-            Sign in with Facebook
-          </button>
-        </div>
-      )}
-    </>
-  );
+  if (!loading && !session) {
+    return (
+      <>
+        {!session && (
+          <div className="min-h-screen bg-gray-100 flex">
+            <div className="flex-1 flex flex-col justify-center relative py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+              <div className="mx-auto w-full justify-between max-w-sm lg:w-84">
+                <div className="flex flex-col items-between">
+                  <h2 className="text-5xl mb-16 font-extrabold text-center text-gray-900">Login</h2>
+
+                  <button
+                    onClick={() => signIn("google")}
+                    className="px-8 mb-4 hover:opacity-70 text-black rounded-md py-4 bg-white shadow-lg flex items-center justify-start"
+                  >
+                    <img src="images/google-icon.svg" className="w-6 w-8 mr-3"></img>
+                    Sign In With Google
+                  </button>
+                  <button
+                    onClick={() => signIn("facebook")}
+                    className="px-8 hover:opacity-80 text-white rounded-md py-4 shadow-lg flex items-center justify-start"
+                    style={{ background: "#3b5998" }}
+                  >
+                    <img src="images/facebook-icon.svg" className="w-6 w-8 mr-3"></img>
+                    Sign In With Facebook
+                  </button>
+                </div>
+                <div className="absolute bottom-4 left-0 w-full flex justify-center">
+                  <img className="w-18" src="images/logo.svg" alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="hidden lg:block relative w-0 flex-1">
+              <img
+                className="absolute inset-0 h-full w-full object-cover"
+                src="images/login-cover.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        )}
+      </>
+    );
+  } else {
+    return null;
+  }
 }
